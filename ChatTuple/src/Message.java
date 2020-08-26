c00790104d3700ded7b1b3760438378e860240e1
Matheus <matheusdamascenotx@gmail.com>
2020-08-25 23:21:08 -0300

    adicionando exemplo de tupla

---
+++ ChatTuple/src/Message.java
@@ -0,0 +1,11 @@
+import net.jini.core.entry.Entry;
+
+public class Message implements Entry {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public String content;
+    public Message() {
+    }
+}
