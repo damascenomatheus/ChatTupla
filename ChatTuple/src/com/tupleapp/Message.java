6c092390341c5d24c4c151f676a5fbb67ffd4c51
Matheus <matheusdamascenotx@gmail.com>
2020-08-26 00:41:09 -0300

    rodei o exemplo do professor

---
+++ ChatTuple/src/com/tupleapp/Message.java
@@ -0,0 +1,12 @@
+package com.tupleapp;
+import net.jini.core.entry.Entry;
+
+public class Message implements Entry {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String content;
+    public Message() {
+    }
+}
