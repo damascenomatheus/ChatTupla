ae2d61f4c21c7a9604cb1337a211dd4663743ed7
Matheus <matheusdamascenotx@gmail.com>
2020-08-27 23:33:38 -0300

    finalizando crianção de usuário

--- ChatTuple/src/com/tupleapp/Principal.java
+++ ChatTuple/src/com/tupleapp/Principal.java
@@ -1,6 +1,7 @@
 package com.tupleapp;
 import java.util.Scanner;
-
+import net.jini.core.entry.Entry;
+import net.jini.core.lease.Lease;
 import net.jini.space.JavaSpace;
 
 public class Principal {
@@ -12,24 +13,76 @@ public class Principal {
              System.exit(-1);
          } 
          
-
-         
+         criaUsuario(space);
+        
+         while(true) {
+        	 System.out.println("1 - Criar Sala");
+             System.out.println("2 - Listar Salas");
+             System.out.print("Digite o número que deseja: ");
+             Scanner scanner = new Scanner(System.in);
+             String message = scanner.nextLine();
+                 
+            switch (message) {
+    		case "1": {
+    			criaSala(space);
+    			break;
+    		}
+    		case "2": {
+    			listaSalas(space);
+    			break;
+    		}
+    		default:
+    			throw new IllegalArgumentException("Unexpected value: " + message);
+    		}
+         }
 	}
 	
-	public void criaSala(JavaSpace space) {
+	public static void criaSala(JavaSpace space) {
 		Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Entre com o texto da mensagem (ENTER para sair): ");
-            String message = scanner.nextLine();
-            if (message == null || message.equals("")) {
-                System.exit(0);
-            }
-            Sala sala = new Sala(message, null);
-            try { 
-           	 space.write(sala, null, 60 * 1000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        System.out.print("Digite o nome da Sala: ");
+        String message = scanner.nextLine();
+        if (message == null || message.equals("")) {
+            System.exit(0);
         }
+        Sala sala = new Sala(message, null, null);
+        System.out.println("criou a sala: " + message);
+        try { 
+       	 space.write(sala, null, 60 * 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void criaUsuario(JavaSpace space) {
+		System.out.print("Digite seu nome: ");
+		Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        
+        if (message == null || message.equals("")) {
+            System.exit(0);
+        }
+        Usuario user = new Usuario(message.toLowerCase(), null);
+        
+        try {
+        	Entry retorno = space.readIfExists(user, null, 60 * 1000);
+        	if (retorno == null) {
+        		System.out.println("\nCriou o usuario: " + message + "\n");
+                
+                try { 
+               	 space.write(user, null, Lease.FOREVER);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        	} else {
+        		System.out.println("\nUsuario já cadastrado\n");
+        		criaUsuario(space);
+        	}
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+	}
+	
+	public static void listaSalas(JavaSpace space) {
+		System.out.println("\nTem sala\n");
 	}
 }
--- ChatTuple/src/com/tupleapp/Usuario.java
+++ ChatTuple/src/com/tupleapp/Usuario.java
@@ -2,19 +2,17 @@ package com.tupleapp;
 import java.util.List;
 import net.jini.core.entry.Entry;
 
-public class Sala implements Entry {
+public class Usuario implements Entry {
 	private static final long serialVersionUID = 1L;
 	public String nome;
-	public List<String> contatos;
 	public List<String> mensagens;
 	
-    public Sala(String nome, List<String> contatos, List<String> mensagens) {
+    public Usuario(String nome, List<String> mensagens) {
     	this.nome = nome;
-    	this.contatos = contatos;
     	this.mensagens = mensagens;
     }
     
-    public Sala() {
+    public Usuario() {
     	
     }
 }
