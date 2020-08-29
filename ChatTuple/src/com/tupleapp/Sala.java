a38cd7093695a0fbe6b21fcb94bd09c1cd76b7d9
Matheus <matheusdamascenotx@gmail.com>
2020-08-29 14:40:49 -0300

    modificando lista das salas de uma lista de string para lista de usuarios

--- ChatTuple/src/com/tupleapp/Sala.java
+++ ChatTuple/src/com/tupleapp/Sala.java
@@ -5,10 +5,10 @@ import net.jini.core.entry.Entry;
 public class Sala implements Entry {
 	private static final long serialVersionUID = 1L;
 	public String nome;
-	public List<String> contatos;
+	public List<Usuario> contatos;
 	public List<String> mensagens;
 	
-    public Sala(String nome, List<String> contatos, List<String> mensagens) {
+    public Sala(String nome, List<Usuario> contatos, List<String> mensagens) {
     	this.nome = nome;
     	this.contatos = contatos;
     	this.mensagens = mensagens;
